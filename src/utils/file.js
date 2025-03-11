f3707e81f9de9ae8194df8793cff25c8dc796970
ygywzb <3119587257@qq.com>
2025-03-12 00:06:09 +0800

    添加注释

--- src/utils/file.js
+++ src/utils/file.js
@@ -1,11 +1,27 @@
 import JSZip from "jszip";
 import { saveAs } from "file-saver";
 
-export const zipFiles = async (files) => {
-    const zip = new JSZip();
-    files.forEach(({ name, content }) => {
-        zip.file(name, content);
-    });
-    const content = await zip.generateAsync({ type: "blob" });
-    saveAs(content, "archive.zip");
-    };
+/**
+ * @typedef {Object} FileData
+ * @property {string} name
+ * @property {*} content
+ */
+
+/**
+ * 将文件列表保存至压缩包中
+ * @param {FileData[]} files 文件列表
+ * @param {string} aname 压缩包命名
+ * @param {string} extention 压缩包拓展名
+ */
+export const zipFiles = async (
+  files,
+  aname = "archive.zip",
+  extention = "zip"
+) => {
+  const zip = new JSZip();
+  files.forEach(({ name, content }) => {
+    zip.file(name, content);
+  });
+  const content = await zip.generateAsync({ type: "blob" });
+  saveAs(content, "".concat(aname, ".", extention));
+};

2f71ce88da76efc82d6fd3c7e09a283f8f6565d1
ygywzb <3119587257@qq.com>
2025-03-25 19:08:24 +0800

    feat: 添加获取最终链接函数回调

--- src/rpc/task.js
+++ src/rpc/task.js
@@ -5,13 +5,45 @@
  */
 
 /**
+ * 真实下载链接获取函数
+ * @callback RealUrlGenFunc
+ * @param {string} nowUrl
+ * @returns {Promise<string>} realUrl
+ */
+
+/**
+ * @typedef {Object} RPCSendOption
+ * @property {Object} rpcOpts RPC需要的额外参数
+ * @property {RealUrlGenFunc} realUrlFunc 若不是最终下载链接，请提供获取最终链接获取函数吧
+ */
+
+/**
+ * 默认配置，默认无需跳转，且rpc无其他要求
+ * @type {RPCSendOption}
+ */
+const defaultRPCSendOption = {
+  rpcOpts: {},
+  realUrlFunc: null,
+};
+
+/**
  * 将一个下载任务推送至aria2 RPC
  * @param {FileInfo} fileInfo 下载文件详情
+ * @param {RPCSendOption} [options] 下载选项，脚本内部设置
  * @returns { Promise<string> } GID
  */
-const sendOneTask = async (fileInfo) => {
+const sendOneTask = async (fileInfo, options = {}) => {
   const { url, filename } = fileInfo;
+  const { rpcOpts, realUrlFunc } = {
+    ...defaultRPCSendOption,
+    ...options,
+  };
   const rpcUrl = 'http://localhost:6800/jsonrpc'; // Aria2 RPC 地址
+  // 获取下载链接
+  let realUrl = url;
+  if (realUrlFunc) {
+    realUrl = await realUrlFunc(url);
+  }
 
   const requestData = {
     jsonrpc: '2.0',
@@ -21,6 +53,7 @@ const sendOneTask = async (fileInfo) => {
       [url],
       {
         out: filename, // 文件名
+        ...rpcOpts,
       },
     ],
   };
@@ -42,14 +75,15 @@ const sendOneTask = async (fileInfo) => {
 /**
  * 通过RPC发送一系列下载任务，并获取所有GID
  * @param {FileInfo[]} infos 下载文件信息列表
+ * @param {RPCSendOption} [options] 下载选项，脚本内部设置
  * @returns {Promise<string[]>}
  */
-export const sendAllTasks = async (infos) => {
+export const sendAllTasks = async (infos, options = {}) => {
   // 收集所有gid
   const gIdList = [];
   for (const info of infos) {
     try {
-      const gid = await sendOneTask(info);
+      const gid = await sendOneTask(info, options);
       gIdList.push(gid);
     } catch (e) {
       console.warn(`任务发送失败，${e}`);
